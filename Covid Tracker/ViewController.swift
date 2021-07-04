2f367b7d7493e5350a3236054b14cceab30fbcf6
mugabe richard <mugabe.richard.37@gmail.com>
2021-07-04 12:38:05 +0200

    Initial Commit

---
+++ Covid Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid Tracker
+//
+//  Created by MUGABE RICHARD on 04/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
